c706d73177a0115bcbd0bfcef1631231456f02ef
GitHub Action <action@github.com>
2021-07-30 02:07:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/wasdjkl/ideanotepad/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/wasdjkl/ideanotepad/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wasdjkl.ideanotepad.listeners
 
+import com.github.wasdjkl.ideanotepad.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/wasdjkl/ideanotepad/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wasdjkl/ideanotepad/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wasdjkl.ideanotepad.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wasdjkl.ideanotepad.MyBundle
 
 class MyApplicationService {
 
